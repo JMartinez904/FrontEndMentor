1140c706afda99df6a0359f7b55e29229fb10ee5
JMartinez904 <josemagiboo@gmail.com>
2022-09-18 18:06:20 -0400

    NFT preview card component

---
+++ #4/nft-preview-card-component-main/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        // Configure your color palette here
+         'border-line':"hsl(215, 32%, 27%)",
+        },
+    },
+  },
+  plugins: [],
+}
